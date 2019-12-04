04a3ce47ee60b218a28ee8e7fa65dad9646ec2df
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-04 01:03:33 -0600

    Add: Select statement to connection.php

--- inc/connections.php
+++ inc/connections.php
@@ -5,12 +5,20 @@ try {
 $db = new PDO("sqlite:".__DIR__."/database.db");
 //Always throw an exception
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-echo "Connected to the database";
-exit;
 }catch(Exception $e){
     echo "Unable to connect";
-    echo $e->getMessage();
+    //echo $e->getMessage();
     exit;
 }
+
+try {
+//Specify a title and category query from media
+   $results = $db->query("SELECT title, category FROM Media");
+   echo "Retrieved Results";
+} catch (Exception $e) {
+    echo "Unable to retrieve results";
+    exit;
+}
+
 
 ?>
