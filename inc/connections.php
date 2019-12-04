9eff66d22908b0e976a5147e7945649aada90553
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-04 17:40:56 -0600

    Test: Fetch_ASSOC as arg in fetchALL method called on the $results object

--- inc/connections.php
+++ inc/connections.php
@@ -13,13 +13,13 @@ $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 try {
 //Specify a title and category query from media
-   $results = $db->query("SELECT title, category FROM Media");
+   $results = $db->query("SELECT title, category, img FROM Media");
 } catch (Exception $e) {
     echo "Unable to retrieve results";
     exit;
 }
 
-var_dump($results->fetchALL());
+var_dump($results->fetchALL(PDO::FETCH_ASSOC));
 
 
 ?>
