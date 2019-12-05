ae9b5bd1e0a3aa76a81174a50f1cf2806b3045f0
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-04 18:04:50 -0600

    Modify: Generate $catalog array PDO object

--- inc/data.php
+++ inc/data.php
@@ -1,4 +1,8 @@
 <?php
+//Include connection.php
+include("connections.php");
+
+/*
 $catalog = [];
 //Books
 $catalog[101] = [
@@ -184,3 +188,4 @@ $catalog[304] = [
     "category" => "Music",
     "artist" => "Nat King Cole"
 ];
+*/
