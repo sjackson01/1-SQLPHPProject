fc69d0153ffb089aea30c97de63b104e8b38f674
Steven Jackson <stevenpjackson0@gmail.com>
2019-12-12 23:21:26 -0600

    Add: Variable containing how many items to show per page. Add:  Conditional that filters input and display only one item if $current_page variable is empty

--- catalog.php
+++ catalog.php
@@ -4,6 +4,8 @@ include("inc/functions.php");
 
 $pageTitle = "Full Catalog";
 $section = null;
+//Set items to page to 8
+$item_per_page = 8;
 
 if (isset($_GET["cat"])) {
     if ($_GET["cat"] == "books") {
@@ -16,6 +18,17 @@ if (isset($_GET["cat"])) {
         $pageTitle = "Music";
         $section = "music";
     }
+}
+
+//If is set get page
+if(isset($_GET["pg"])){
+    //Filter and sanitize current page sent through $_GET
+    $current_page = filter_input(INPUT_GET,"pg",FILTER_SANITIZE_NUMBER_INT);
+}
+
+//If current page is not set, set to one
+if(empty($current_page)){
+    $current_page = 1;
 }
 
 //Category conditional
